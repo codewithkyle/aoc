f073ea0065214f1497f5d2ffc43241ed68c05079
Kyle Andrews <codingwithkyle@gmail.com>
2025-12-05 19:45:32 -0500

    2025/04 - part 2

--- 2025/04/main.c
+++ 2025/04/main.c
@@ -2,6 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+int part_2(int *grid, int rows, int cols)
+{
+    int total = 0;
+    while (1)
+    {
+        int sum = 0;
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < cols; x++)
+            {
+                size_t idx = y * cols + x;
+                if (grid[idx] == 0) continue;
+                int adj_sum = 0;
+                if (x > 0 && grid[y * cols + (x-1)]) adj_sum++;
+                if (x < cols-1 && grid[y * cols + (x+1)]) adj_sum++;
+
+                if (y > 0 && grid[(y-1) * cols + x]) adj_sum++;
+                if (y < rows-1 && grid[(y+1) * cols + x]) adj_sum++;
+
+                if (y > 0 && x > 0 && grid[(y-1) * cols + (x-1)]) adj_sum++;
+                if (y < rows-1 && x < cols-1 && grid[(y+1) * cols + (x+1)]) adj_sum++;
+
+                if (y > 0 && x < cols-1 && grid[(y-1) * cols + (x+1)]) adj_sum++;
+                if (y < rows-1 && x > 0 && grid[(y+1) * cols + (x-1)]) adj_sum++;
+
+                if (adj_sum < 4) 
+                {
+                    grid[idx] = 0;
+                    sum++;
+                }
+            }
+        }
+        if (sum == 0) break;
+        total += sum;
+    }
+    return total;
+}
+
 int part_1(int *grid, int rows, int cols)
 {
     int sum = 0;
@@ -65,10 +103,12 @@ int main()
     }
 
     int part_1_sum = part_1(grid, rows, cols);
+    int part_2_sum = part_2(grid, rows, cols);
 
     fclose(f);
     free(grid);
     printf("Part 1: %i\n", part_1_sum);
+    printf("Part 2: %i\n", part_2_sum);
 
     return 0;
 }
