334b9015148fc2e08f71e2d5c56caffe36dbc92b
Kyle Andrews <codingwithkyle@gmail.com>
2025-12-05 19:29:08 -0500

    2025/04 - part 1

---
+++ 2025/04/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int part_1(int *grid, int rows, int cols)
+{
+    int sum = 0;
+    for (int y = 0; y < rows; y++)
+    {
+        for (int x = 0; x < cols; x++)
+        {
+            if (grid[y * cols + x] == 0) continue;
+            int adj_sum = 0;
+            if (x > 0 && grid[y * cols + (x-1)]) adj_sum++;
+            if (x < cols-1 && grid[y * cols + (x+1)]) adj_sum++;
+
+            if (y > 0 && grid[(y-1) * cols + x]) adj_sum++;
+            if (y < rows-1 && grid[(y+1) * cols + x]) adj_sum++;
+
+            if (y > 0 && x > 0 && grid[(y-1) * cols + (x-1)]) adj_sum++;
+            if (y < rows-1 && x < cols-1 && grid[(y+1) * cols + (x+1)]) adj_sum++;
+
+            if (y > 0 && x < cols-1 && grid[(y-1) * cols + (x+1)]) adj_sum++;
+            if (y < rows-1 && x > 0 && grid[(y+1) * cols + (x-1)]) adj_sum++;
+
+            if (adj_sum < 4) sum++;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    FILE *f = fopen("data.txt", "r");
+    if (!f)
+    {
+        perror("failed to open file");
+        return -1;
+    }
+
+    int rows = 0;
+    int cols = 0;
+    char line[4096];
+    while(fgets(line, sizeof(line), f))
+    {
+        if (cols == 0)
+        {
+            cols = strlen(line)-1;
+        }
+        rows++;
+    }
+    rewind(f);
+    int *grid = calloc(cols * rows, sizeof(int));
+    int y = 0;
+    while(fgets(line, sizeof(line), f))
+    {
+        for (size_t x = 0; x < cols; x++)
+        {
+            if (line[x] == '@')
+            {
+                grid[y * cols + x] = 1;
+            }
+        }
+        y++;
+    }
+
+    int part_1_sum = part_1(grid, rows, cols);
+
+    fclose(f);
+    free(grid);
+    printf("Part 1: %i\n", part_1_sum);
+
+    return 0;
+}
